cd0cb5c064bd25ee1ca2fe07df135f6134bb5ed8
karrycharon <karrycharon@qq.com>
2024-01-12 11:25:29 +0800

    add node btn

--- web/js/loader/enterLoader.js
+++ web/js/loader/enterLoader.js
@@ -8,67 +8,76 @@ function enterLoader() {
   window.addEventListener("message", message);
 }
 
+function callBack() {
+  console.log("enterLoader", this._node);
+  window.removeEventListener("message", message);
+  const html = `<iframe id="loader_iframe" src="./index.html" frameborder="0">
+  </iframe>`;
+  // window.postMessage({ type: "set_node" }, this._node);
+  document.body.insertAdjacentHTML("beforeend", html);
+  let w = document.getElementById("loader_iframe").contentWindow;
+  w._node = this._node;
+  window.addEventListener("message", message);
+}
+
 function message(event) {
   if (event.data.type === "close_loader_page") {
     document.body.removeChild(document.getElementById("loader_iframe"));
   }
 }
 
+function registerCallBack(node) {
+  if (node.constructor.comfyClass == "CheckpointLoaderSimple") {
+    let btn = node.addWidget("button", "模型管理器", "MMM", callBack);
+    btn._node = node;
+    const f = node.onSerialize;
+    function onSerialize(o) {
+      if (f) {
+        f.apply(this, arguments);
+      }
+      for (var i = 0; i < this.widgets.length; ++i) {
+        if (this.widgets[i] == btn) {
+          delete o.widgets_values[i];
+          o.widgets_values = o.widgets_values.filter((v) => v);
+          break
+        }
+      }
+    }
+    node.onSerialize = onSerialize;
+  }
+}
+
+function styleInit() {
+  const style = document.createElement("style");
+  style.type = "text/css"; // 已启用 需要更改
+  style.innerHTML = `
+    iframe {
+      position: absolute;
+      left: 0;
+      right: 0;
+      width: 100%;
+      height: 100%;
+      z-index: 9999;
+      background: #000000;
+    }
+    .cs {
+      position: absolute;
+      left: 0;
+      right: 0;
+      width: 5vw;
+      height: 5vw;
+      background: #00ffff;
+    }
+  `;
+  document.head.appendChild(style);
+}
+
 const ext = {
   name: "AIGODLIKE.MMM",
   async init(app) {
-    // Any initial setup to run as soon as the page loads
-    /* 定义style:
-      <style type="text/css">
-      iframe {
-        position: absolute;
-        left: 0;
-        right: 0;
-        width: 100%;
-        height: 100%;
-        z-index: 9999;
-        background: #000000;
-      }
-    
-      .cs {
-        position: absolute;
-        left: 0;
-        right: 0;
-        width: 5vw;
-        height: 5vw;
-        background: #00ffff;
-      }
-      </style>
-    */
-    const style = document.createElement("style");
-    style.type = "text/css"; // 已启用 需要更改
-    style.innerHTML = `
-      iframe {
-        position: absolute;
-        left: 0;
-        right: 0;
-        width: 100%;
-        height: 100%;
-        z-index: 9999;
-        background: #000000;
-      }
-      .cs {
-        position: absolute;
-        left: 0;
-        right: 0;
-        width: 5vw;
-        height: 5vw;
-        background: #00ffff;
-      }
-    `;
-    document.head.appendChild(style);
-    // 在当前页面添加一个按钮
-    const html = `<button type="button" class="cs">加载器</button>`;
-    document.body.insertAdjacentHTML("beforeend", html);
-    // 设置按钮button绑定的事件 为enterLoader()
-    document.querySelector(".cs").addEventListener("click", enterLoader);
+    styleInit();
   },
-  async setup(app) {},
+  async setup(app) { },
   async addCustomNodeDefs(defs, app) {
     // Add custom node definitions
     // These definitions will be configured and registered automatically
@@ -91,8 +100,12 @@ const ext = {
     // Register any custom node implementations here allowing for more flexability than a custom node def
     // console.log("[logging]", "register custom nodes");
   },
-  loadedGraphNode(node, app) {},
-  nodeCreated(node, app) {},
+  loadedGraphNode(node, app) {
+    // registerCallBack(node);
+  },
+  nodeCreated(node, app) {
+    registerCallBack(node);
+  },
 };
 
 app.registerExtension(ext);
