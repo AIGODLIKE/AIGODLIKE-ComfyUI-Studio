86f7e851afc818289f51c236b570ccba1cf4642a
SHAO <1293110016@qq.com>
2024-02-21 16:57:25 +0800

    Feat: Add Display Column Number

--- loader/static/js/i18n.js
+++ loader/static/js/i18n.js
@@ -66,6 +66,18 @@ const message = {
         ],
         sizeList: [
           {
+            name: "9列",
+            value: 9,
+          },
+          {
+            name: "8列",
+            value: 8,
+          },
+          {
+            name: "7列",
+            value: 7,
+          },
+          {
             name: "6列",
             value: 6,
           },
@@ -229,6 +241,18 @@ const message = {
         ],
         sizeList: [
           {
+            name: "9 COLUMN",
+            value: 9,
+          },
+          {
+            name: "8 COLUMN",
+            value: 8,
+          },
+          {
+            name: "7 COLUMN",
+            value: 7,
+          },
+          {
             name: "6 COLUMN",
             value: 6,
           },
@@ -391,6 +415,18 @@ const message = {
           },
         ],
         sizeList: [
+          {
+            name: "9列",
+            value: 9,
+          },
+          {
+            name: "8列",
+            value: 8,
+          },
+          {
+            name: "7列",
+            value: 7,
+          },
           {
             name: "6列",
             value: 6,
