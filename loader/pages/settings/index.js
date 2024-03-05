1b87e125c91b4e3edb350bf58f6de98ee4ba2ef3
SHAO <1293110016@qq.com>
2024-03-05 17:43:40 +0800

    Feat: Add Full Screen And Windowing Option

--- loader/pages/settings/index.js
+++ loader/pages/settings/index.js
@@ -1,11 +1,13 @@
 import Language from "../../components/settings/language/index.js";
 import ModelShield from "../../components/settings/modelShielding/index.js";
+import windowing from "../../components/settings/windowing/index.js";
 
 export default {
   name: "Settings",
   components: {
     Language,
     ModelShield,
+    windowing,
   },
   data() {
     return {};
@@ -16,6 +18,7 @@ export default {
                 <div class="content">
                   <Language />
                   <ModelShield />
+                  <windowing />
                 </div>
               </div>
               <div class="foot"></div>
--- loader/store/config.js
+++ loader/store/config.js
@@ -2,11 +2,21 @@ const config = {
   namespaced: true,
   state: () => ({
     language: "cn",
+    windowing: localStorage.getItem("windowing") || "full",
   }),
   mutations: {
     updateLanguage(state, newLanguage) {
       localStorage.setItem("language", newLanguage);
       state.language = newLanguage;
+    },
+    updateWindowing(state, value) {
+      localStorage.setItem("windowing", value);
+      if (value === "full") {
+        document.getElementById("loader_page").classList.remove("windowing");
+      } else {
+        document.getElementById("loader_page").classList.add("windowing");
+      }
+      state.windowing = value;
     },
   },
 };
