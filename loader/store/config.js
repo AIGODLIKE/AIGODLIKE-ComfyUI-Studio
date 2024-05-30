760c89a959cee0b8240d90b23ad6597e47d1f85b
KarryCharon <karrycharon@qq.com>
2024-05-30 10:13:38 +0800

    fix setting config save

--- loader/store/config.js
+++ loader/store/config.js
@@ -1,21 +1,21 @@
 const config = {
   namespaced: true,
   state: () => ({
-    language: "cn",
-    shortcut: "click",
-    windowing: localStorage.getItem("windowing") || "full",
+    language: localStorage.getItem("CS.language") || "cn",
+    shortcut: localStorage.getItem("CS.shortcut") || "click",
+    windowing: localStorage.getItem("CS.windowing") || "full",
   }),
   mutations: {
     updateLanguage(state, newLanguage) {
-      localStorage.setItem("language", newLanguage);
+      localStorage.setItem("CS.language", newLanguage);
       state.language = newLanguage;
     },
     updateShortcut(state, newShortcut) {
-      localStorage.setItem("shortcut", newShortcut);
+      localStorage.setItem("CS.shortcut", newShortcut);
       state.shortcut = newShortcut;
     },
     updateWindowing(state, value) {
-      localStorage.setItem("windowing", value);
+      localStorage.setItem("CS.windowing", value);
       if (value === "full") {
         document.getElementById("loader_page").classList.remove("windowing");
       } else {
